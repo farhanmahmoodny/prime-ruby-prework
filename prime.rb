51d39f5c2dcb62843eeb2c306ef36c3b0daf1e52
Farhan Mahmood <farhanmahmoodny@gmail.com>
2019-01-12 05:49:40 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -2,9 +2,9 @@
 def prime?(number)
   if number <= 0 
     return false
-  elsif number % 2 == 0
+  elsif number % 2 == 0 && number > 3
     return false
-  elsif number % 3 == 0
+  elsif number % 3 == 0 && number > 5
     return false
   elsif number % 41 == 0
     return false
