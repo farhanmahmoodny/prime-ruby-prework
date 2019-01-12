f0f0fdb80b274966974f07300a896a447a23d344
Farhan Mahmood <farhanmahmoodny@gmail.com>
2019-01-12 05:48:58 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -1,14 +1,12 @@
 # Add  code here!
 def prime?(number)
-  if number < 4
-    return true
+  if number <= 0 
+    return false
   elsif number % 2 == 0
     return false
   elsif number % 3 == 0
     return false
   elsif number % 41 == 0
-    return false
-  elsif number < 0
     return false
   else
     return true
