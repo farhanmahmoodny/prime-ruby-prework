8debfae96e20b2c8bf60251f0bbf816226eb5c0c
Farhan Mahmood <farhanmahmoodny@gmail.com>
2019-01-12 05:11:50 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -1,4 +1,18 @@
 # Add  code here!
 def prime?(number)
-  
+  def div_2
+  if number % 2 = 0 && number > 3
+    return true
+  else
+    return false
+  end
+end
+  def div_3
+  if number % 3 && number > 5
+    return true
+  else
+    return false
+  end
+end
+
 end
