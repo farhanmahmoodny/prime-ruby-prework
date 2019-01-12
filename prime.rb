5f4f2af79a796304e4e516a06fb407849ec72cc9
Farhan Mahmood <farhanmahmoodny@gmail.com>
2019-01-12 05:29:25 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -1,8 +1,8 @@
 # Add  code here!
 def prime?(number)
-  if number > 3 && number % 2 == 0
+  if number % 2 == 0
     return false
-  elsif number > 5 && number % 3 == 0
+  elsif number % 3 == 0
     return false
   elsif number <= 0
     return false
