287fd4da198c26659457de637e4cbae89eef319e
Farhan Mahmood <farhanmahmoodny@gmail.com>
2019-01-12 05:17:45 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -1,21 +1,4 @@
 # Add  code here!
 def prime?(number)
-  def div_2(number)
-  if number % 2 == 0 && number > 3
-    return true
-  else
-    return false
-  end
-end
-  def div_3(number)
-  if number % 3 == 0 && number > 5
-    return true
-  else
-    return false
-  end
-end
-while number > 0
-  div_2(number)
-  div_3(number)
-end
+while number > 3
 end
