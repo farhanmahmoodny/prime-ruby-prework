5b9eadd88e4cb8ffc13f62651a7406a6c3e87843
Farhan Mahmood <farhanmahmoodny@gmail.com>
2019-01-12 05:24:29 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -1,10 +1,11 @@
 # Add  code here!
 def prime?(number)
-  while number > 3
+  until number > 3
   if number % 2 == 0
     return false
   elsif number % 3 == 0
     return false
-  else return true
+  else 
+    return true
   end
 end
